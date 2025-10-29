df9f2de2681cfcba72707df51ba4fec71555f6f7
Yamato <z201v@yandex.ru>
2025-10-29 17:26:06 +0700

    change urls.py

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,4 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
@@ -19,4 +21,9 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
     search_fields = ['products__title', 'products__description']
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Hello students'})
+
 
